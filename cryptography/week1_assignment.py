0062e28e60acf8e890b57403e8e01629283ac76c
dfashch <d.faschenko@gmail.com>
2020-03-16 01:12:47 +0300

    week1 minor

--- cryptography/week1_assignment.py
+++ cryptography/week1_assignment.py
@@ -16,24 +16,32 @@ MSGS = ('315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146f
 
 def strxor(a, b):     # xor two strings of different lengths
     if len(a) > len(b):
-       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+       # return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+       return "".join(
+           [
+               chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)
+           ]
+       )
     else:
        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
 
 def random(size=16):
-    return open("/dev/urandom").read(size)
+    return open("/dev/urandom", 'rb').read(size)
 
 
 def encrypt(key, msg):
+    print('-'*30)
     c = strxor(key, msg)
-    print()
-    print(c.encode('hex'))
+    print(f'c={c}')
+    # print(c.encode('hex'))
     return c
 
 
 def main():
-    key = random(1024)
+    key = str(random(1024))
     ciphertexts = [encrypt(key, msg) for msg in MSGS]
-    print(ciphertexts)
+    # print(ciphertexts)
 
+
+main()
