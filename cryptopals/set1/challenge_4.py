34e7641122705c0b7a9aca293f343b1e63038129
dzmitry_fashchanka <dzmitry_fashchanka@goodyear.com>
2024-10-09 10:19:50 +0300

    4 chal completed

--- cryptopals/set1/challenge_4.py
+++ cryptopals/set1/challenge_4.py
@@ -7,11 +7,12 @@ from os import strerror
 #  One of the 60-character strings in this file has been encrypted by single-character XOR.
 #  Find it.
 print('-' * 100)
-print('-', f'Challenge 1 - Detect single-character XOR:')
+print('-', f'Challenge 4 - Detect single-character XOR:')
 print('-' * 100)
 
 in_file = 'challenge4.txt'
-
+all_list = [ ' ', '!', '#', '$', '%', '&', '`', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~' ]
+letters_list = [ ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 result_count = 0
 result_key = ''
 result_msg = ''
@@ -23,17 +24,11 @@ def xor(bytes_strA, bytes_strB):
 
 def check_if_hex_enc_with_one_char(hex_str):
     global result_count
+    global result_key
+    global result_msg
     bytes_str = bytes.fromhex(hex_str)
     # print(f'BYTES: {bytes_str}')
     bytes_str_len = len(bytes_str)
-    all_list = [' ', '!', '#', '$', '%', '&', '`', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5',
-                '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', 'a',
-                'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                'w', 'x', 'y', 'z', '{', '|', '}', '~']
-    letters_list = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     cnt = Counter()
 
@@ -46,8 +41,6 @@ def check_if_hex_enc_with_one_char(hex_str):
             text_result = result.decode()
         except UnicodeDecodeError:
             continue
-        # text_result = result.decode('latin-1')
-        # print(f'  - {text_result}')
         # count letters in results
         for ch1 in text_result:
             if ch1 in letters_list:
@@ -61,14 +54,12 @@ def check_if_hex_enc_with_one_char(hex_str):
             tmp_msg = tmp_bytes.decode()
         except UnicodeDecodeError:
             continue
-        print(f'RESULT: {frequency} - {tmp_key} - {tmp_msg}')
+        # print(f'TMP: {frequency} - {tmp_key} - {tmp_msg}')
 
         if (frequency > result_count):
             result_count = frequency
             result_key = item
             result_msg = tmp_msg
-
-
 
 
 #-----------------------------------------------------------------------------------------
@@ -80,18 +71,13 @@ try:
     lcnt = 0
     for line in open(in_file, 'rt', encoding='latin-1'):
         lcnt += 1
-        # if (lcnt == 3):
-        #     break
-        print(f'{lcnt} - {len(line)} : {line}')
-        # print(f'        {bytes.fromhex(line)}')
-        # CODE HERE
         check_if_hex_enc_with_one_char(line)
 
     print("Lines in file:       ", lcnt)
 except IOError as e:
     print("I/O error occured: ", strerror(e.errno))
 
-print(f'{result_count} - {result_key} - {result_msg}')
+print(f'RESULT: {result_count} - {result_key} - {result_msg}')
 
 
 
